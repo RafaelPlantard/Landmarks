6d23cd1bcc4a692846c9e95c9281086590e88db8
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2021-02-11 22:12:24 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rafael Ferreira on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
